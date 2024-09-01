ce04db378db530f099bfcb42cd7886bfa25fe634
noor <cornerit003@gmail.com>
2024-09-01 13:58:15 +0500

    ...

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import datetime
 
 URL = 'https://heritagejewels.com.pk/'
 r = requests.get(URL)
@@ -20,14 +21,16 @@ if table:
             quote['price'] = price_div.text.strip()
         else:
             quote['price'] = 'Price not found'
-            
-        rating=row.find('div', class_='t4s-product-price')
+    
+        quote['rating']=row.find('span', class_="jdgm-prev-badge__text").text.strip().replace("reviews", '')
+        
 
         data.append(quote)
 
 df = pd.DataFrame(data)
 
-print(df)
-# df.to_csv('heritage_jewels_best_sellers2.csv', index=False)
+current_time = str(datetime.datetime.now())
 
-# print("Data written to heritage_jewels_best_sellers.csv")
+df.to_csv('file_' + current_time + '.csv', index=False)
+
+print("Data written to heritage_jewels_best_sellers.csv")
